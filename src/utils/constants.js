56ff20d2af290b7f3a89f4ab4d1ecc5a222121da
Ahmad Sayahi <33761905+ahmadelectron@users.noreply.github.com>
2026-03-13 22:56:33 +0330

    Delete src directory

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-// src/utils/constants.js
-export const TRANSACTION_TYPES = {
-  INCOME: "income",
-  EXPENSE: "expense",
-};
-
-// export const CATEGORIES = ["Food", "Transport", "Bills", "Other"];
